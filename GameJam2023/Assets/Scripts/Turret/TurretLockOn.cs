6a6029f7de36c7252fb0b9b152d4c58a34c1e39a
H4zard101 <Harry2.Hodgkins@live.uwe.ac.uk>
2023-02-03 23:15:37 +0000

    Fixed the turret collision, also added some enemy tracking

--- GameJam2023/Assets/Scripts/Turret/TurretLockOn.cs
+++ GameJam2023/Assets/Scripts/Turret/TurretLockOn.cs
@@ -4,13 +4,56 @@ using UnityEngine;
 
 public class TurretLockOn : MonoBehaviour
 {
+    public Transform rotationPart;
+    private Transform target;
+    public float range = 10.0F;
 
-    public void OnCollisionStay(Collision collision)
+    private void Start()
     {
-        if(collision.gameObject.tag == "Enemy")
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
         {
-            Debug.Log("Enemy");
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
         }
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (target == null)
+            return;
+
+        Vector3 direction = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = lookRotation.eulerAngles;
+
+        // For some reason the X has to be - overwise the barrel poits up side down 
+        rotationPart.rotation = Quaternion.Euler(-rotation.x, rotation.y, 0f);
 
     }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
 }
