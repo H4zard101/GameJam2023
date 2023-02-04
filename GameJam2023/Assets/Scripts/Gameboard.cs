17564e67c83e3b2c90dd05021daab3ee0fe6e77e
samsquanchone <105938519+samsquanchone@users.noreply.github.com>
2023-02-04 14:23:13 +0000

    Few grid tweaks

--- GameJam2023/Assets/Scripts/Gameboard.cs
+++ GameJam2023/Assets/Scripts/Gameboard.cs
@@ -134,7 +134,7 @@ public class Gameboard : MonoBehaviour
 
     public void PlaceUnit(Unit U, Vector3Int locationToPlace)
     {
-        Instantiate(U.turrets[0], locationToPlace, Quaternion.identity);
+        Instantiate(U.prefab, locationToPlace, Quaternion.identity);
     }
 
 
