e430ff0a6ec3a9a035af7c962bcca9093bbf422b
Mihai Anca <mihai.anca@uwe.ac.uk>
2023-02-04 13:39:58 +0000

    Spawn turrets when upgrade tree

--- GameJam2023/Assets/Scripts/Trees/TreeSource.cs
+++ GameJam2023/Assets/Scripts/Trees/TreeSource.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 public class TreeSource : MonoBehaviour
 {
     public GameObject SourceTree;
+    public GameObject turret_type;
     public GameObject[] turretsUnderInfluence;
-    public int maximumNumberOfTurrets = 3;
-    public int treeSourceLevel = 1;
+    private int treeSourceLevel = 0;
     public float Health = 100;
+    public float HealthPerLevel = 50f;
+    public float gridSize = 3f;
+    private Vector3[] positions = new Vector3[4];
+
+    private void Start()
+    {
+        positions[0] = transform.position;
+        positions[0].x += gridSize;
+        positions[0].y = 0;
+        positions[1] = transform.position;
+        positions[1].x -= gridSize;
+        positions[1].y = 0;
+        positions[2] = transform.position;
+        positions[2].z += gridSize;
+        positions[2].y = 0;
+        positions[3] = transform.position;
+        positions[3].z -= gridSize;
+        positions[3].y = 0;
+
+        Upgrade();
+    }
 
     public void Awake()
     {
         SourceTree = this.gameObject;
-        turretsUnderInfluence = new GameObject[maximumNumberOfTurrets];
+        turretsUnderInfluence = new GameObject[4];
     }
 
     private void Update()
     {
-        if(Health <= 0)
+        if (Health <= 0)
         {
             Destroy(this.gameObject);
             foreach(GameObject turret in turretsUnderInfluence)
@@ -26,6 +47,16 @@ public class TreeSource : MonoBehaviour
                 Destroy(turret);
             }
         }
-        
+    }
+
+    public void Upgrade()
+    {
+        if (treeSourceLevel < 4)
+        {
+            treeSourceLevel += 1;
+            GameObject new_turret = Instantiate(turret_type, positions[treeSourceLevel - 1], Quaternion.identity);
+            turretsUnderInfluence[treeSourceLevel - 1] = new_turret;
+            Health += HealthPerLevel;
+        }
     }
 }
